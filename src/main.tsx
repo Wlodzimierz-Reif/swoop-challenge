f51acc7900f7c7c4685f96f07fe1a43feb22b2d0
w-reif <wlodzimierzreif@gmail.com>
2025-09-14 08:38:20 +0100

    fixed imports and aliases

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import '@radix-ui/themes/styles.css';
 
 import App from './App.tsx';
 import { TodoCatProvider } from '@utils/context.tsx';
-import { ThemeProvider } from 'ThemeProvider.tsx';
+import { ThemeProvider } from './ThemeProvider.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
