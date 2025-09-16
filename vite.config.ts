3cc5dc3a814b9e0da36736fbd955e12365e3525a
Wlodzimierz Reif <wlodzimierzreif@gmail.com>
2025-09-16 20:09:17 +0100

    removed unused code

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,4 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  //   resolve: {
-  //   alias: {
-  //     '@utils': path.resolve(__dirname, './src/utils'),
-  //     '@components': path.resolve(__dirname, './src/components'),
-  //     // add other aliases as needed
-  //   },
-  // },
 })
