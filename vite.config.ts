61c1a47a88ec48a069ab2a88ce6199f9a9618880
Wlodzimierz Reif <wlodzimierzreif@gmail.com>
2025-09-16 20:09:05 +0100

    added vite auto path alias package

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    resolve: {
-    alias: {
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      // add other aliases as needed
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
+  //   resolve: {
+  //   alias: {
+  //     '@utils': path.resolve(__dirname, './src/utils'),
+  //     '@components': path.resolve(__dirname, './src/components'),
+  //     // add other aliases as needed
+  //   },
+  // },
 })
